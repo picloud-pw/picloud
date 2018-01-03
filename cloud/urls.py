3c7b20d326c254ddee84a534f2b5112125d07855
Andrey Zavodov <andrey0398@gmail.com>
2018-01-03 20:46:34 +0300

    Добавлены подробные страницы для постов

--- cloud/urls.py
+++ cloud/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- cloud/views.py
+++ cloud/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'cloud/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'cloud/post_detail.html', {'post': post})
