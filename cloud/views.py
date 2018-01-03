6860fb471e027ffbe7e6662fe455a65e7b2e2980
Andrey Zavodov <andrey0398@gmail.com>
2018-01-03 19:33:28 +0300

    Добавлен querySet для получения постов

--- cloud/views.py
+++ cloud/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'cloud/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'cloud/post_list.html', {'posts': posts})
