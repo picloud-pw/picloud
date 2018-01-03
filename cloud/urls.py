431ebafdc381ea24b307e252ed3eb348ca9500b4
Andrey Zavodov <andrey0398@gmail.com>
2018-01-03 22:03:26 +0300

    Формы для добавления и редактирования постов

--- cloud/urls.py
+++ cloud/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path(r'post/new/', views.post_new, name='post_new'),
+    path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
