7f74b37026fcbdfb3140e1a1457399c950fb4792
Andrey Zavodov <andrey0398@gmail.com>
2018-01-03 17:24:14 +0300

    Первая версия модели для поста

---
+++ Cloud/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=False)
+    text = models.TextField(null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
