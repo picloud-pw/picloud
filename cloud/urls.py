2d3f4e20d35ebacf84a8353f296749fb9aa80e9b
Andrey Zavodov <andrey0398@gmail.com>
2018-01-07 15:00:49 +0300

    Исправлены urls

--- cloud/urls.py
+++ cloud/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    path(r'post/new/', views.post_new, name='post_new'),
-    path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    path(r'^auth/sign_up/', views.sign_up, name="sign_up"),
-    path(r'^auth/sign_in/', views.sign_in, name="sign_in"),
-    path(r'^auth/sign_out/', views.sign_out, name="sign_out"),
-    path(r'^search/', views.search, name="search"),
-    path(r'^memes/', views.memes, name="memes"),
+    re_path(r'post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    re_path(r'post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    path('auth/sign_up/', views.sign_up, name="sign_up"),
+    path('auth/sign_in/', views.sign_in, name="sign_in"),
+    path('auth/sign_out/', views.sign_out, name="sign_out"),
+    path('search/', views.search, name="search"),
+    path('memes/', views.memes, name="memes"),
 ]
