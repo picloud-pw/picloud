386c9a1076fc6c5973794a5324b6e9292d243a80
Andrey Zavodov <andrey0398@gmail.com>
2018-01-11 01:03:41 +0300

    Добавлены констрейнты полей в базу

--- cloud/models.py
+++ cloud/models.py
@@ -11,7 +11,7 @@ class PostType(models.Model):
 
 class University(models.Model):
     title = models.CharField(max_length=256, null=False)
-    logo = models.ImageField(upload_to='resources/u_logo/')
+    logo = models.ImageField(upload_to='resources/u_logo/', null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -34,10 +34,10 @@ class Chair(models.Model):
 
 
 class Lecturer(models.Model):
-    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    department = models.ForeignKey('Department', on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
-    complexity = models.PositiveSmallIntegerField(null=True)
+    complexity = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -45,10 +45,10 @@ class Lecturer(models.Model):
 
 class Subject(models.Model):
     program = models.ForeignKey('Program', on_delete=models.CASCADE)
-    lecturer = models.ForeignKey('Lecturer', on_delete=models.CASCADE)
+    lecturer = models.ForeignKey('Lecturer', on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=256, null=False)
-    short_title = models.CharField(max_length=16, null=False)
-    course = models.PositiveSmallIntegerField(null=True)
+    short_title = models.CharField(max_length=16, null=True, blank=True)
+    course = models.PositiveSmallIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.short_title + " (" + self.title + ")"
@@ -72,23 +72,23 @@ class UserStatus(models.Model):
 
 
 class UserInfo(models.Model):
-    avatar = models.ImageField(upload_to='resources/user_avatars/', null=True)
+    avatar = models.ImageField(upload_to='resources/user_avatars/', null=True, blank=True)
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     status = models.ForeignKey('UserStatus', on_delete=models.CASCADE)
-    program = models.ForeignKey('Program', on_delete=models.CASCADE)
+    program = models.ForeignKey('Program', on_delete=models.CASCADE, null=True, blank=True)
     karma = models.SmallIntegerField(default=10, null=False)
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=256, null=False)
-    text = models.TextField(null=True)
+    text = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
     type = models.ForeignKey('PostType', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='resources/posts/%Y/%m/%d/', null=True)
-    link = models.URLField(max_length=512, null=True)
-    file = models.FileField(upload_to='resources/posts/%Y/%m/%d/', null=True)
+    image = models.ImageField(upload_to='resources/posts/%Y/%m/%d/', null=True, blank=True)
+    link = models.URLField(max_length=512, null=True, blank=True)
+    file = models.FileField(upload_to='resources/posts/%Y/%m/%d/', null=True, blank=True)
     validate_status = models.PositiveSmallIntegerField(default=0)
 
     def publish(self):
