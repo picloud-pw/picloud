8f524ce47349f716dd3a6fc9217dd969ecacadba
Andrey Zavodov <andrey0398@gmail.com>
2018-01-11 01:26:56 +0300

    Модели добавлены в админскую панель

--- cloud/admin.py
+++ cloud/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 
+admin.site.register(University)
+admin.site.register(Department)
+admin.site.register(Chair)
+admin.site.register(Subject)
+admin.site.register(Program)
+admin.site.register(Lecturer)
+
+admin.site.register(UserInfo)
+admin.site.register(UserStatus)
+
+admin.site.register(PostType)
 admin.site.register(Post)
