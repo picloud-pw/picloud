5c325c206860146427eaaf9434164764dea4e75a
Andrey Zavodov <andrey0398@gmail.com>
2018-01-13 03:28:39 +0300

    Добавлены поля в форму для редактирования и добавления поста

--- cloud/forms.py
+++ cloud/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'subject', 'type', 'image', 'link', 'file')
 
 
 class SignupForm(forms.ModelForm):
