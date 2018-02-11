f26e7696281b0a0294da64d160a08b8d13c4d913
Andrey Zavodov <andrey0398@gmail.com>
2018-02-11 03:19:59 +0300

    Добавлены (но не подключены!) скрипты транслита

---
+++ cloud/static/js/filenameToTranslit.js
@@ -0,0 +1,25 @@
+function toTranslit(text) {
+    return text.replace(/([а-яё])|([\s_-])|([^a-z\d])/gi,
+        function (all, ch, space, words, i) {
+            if (space || words) {
+                return space ? '-' : '';
+            }
+            let code = ch.charCodeAt(0),
+                index = code === 1025 || code === 1105 ? 0 :
+                    code > 1071 ? code - 1071 : code - 1039,
+                t = ['yo', 'a', 'b', 'v', 'g', 'd', 'e', 'zh',
+                    'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p',
+                    'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh',
+                    'shch', '', 'y', '', 'e', 'yu', 'ya'
+                ];
+            return t[index];
+        });
+}
+
+document.querySelector('#id_file').onchange = function (e) {
+    files = this.files;
+    for (let i = 0; i < files.length; i++) {
+        alert(toTranslit(files[i].name));
+    }
+
+}
