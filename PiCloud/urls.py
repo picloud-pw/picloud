710462528d9c762f724c5940d20120a21d05479f
Andrey Zavodov <andrey0398@gmail.com>
2018-02-12 17:11:25 +0300

    Добавлен robots.txt и директива для yandex Вебмастера

--- PiCloud/urls.py
+++ PiCloud/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('cloud.urls')),
+    path(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
