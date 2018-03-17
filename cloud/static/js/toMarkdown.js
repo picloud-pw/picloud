3394c8671c2a11174048b7e467adfe422659e9ec
Egor Dubenetskiy <egor@banka.space>
2018-03-18 00:08:04 +0300

    Не рендерить Markdown при загрузке страницы
    Его рендерит сервер.

--- cloud/static/js/toMarkdown.js
+++ cloud/static/js/toMarkdown.js
@@ -1,16 +1,5 @@
 let converter = new showdown.Converter();
 
-$(document).ready(function () {
-    update_markdown();
-});
-
-function update_markdown() {
-    $(".post-text, .post .text").each(function () {
-        let text = $(this).text().replace(/^\s+|\s+$/g, '');
-        $(this).html(converter.makeHtml(text));
-    });
-}
-
 $("#markdownLive").html(converter.makeHtml($("textarea").val()));
 $("textarea").bind('input propertychange', function () {
     $("#markdownLive").html(converter.makeHtml($("textarea").val()));
