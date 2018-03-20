67299c286cb66cc685a22dacd51e974598179847
Egor Dubenetskiy <egor@banka.space>
2018-03-20 07:06:27 +0300

    Добавить ready.js

---
+++ cloud/static/js/ready.js
@@ -0,0 +1,7 @@
+function ready(callback) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        callback();
+    } else {
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+}
