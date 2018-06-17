a9d21bfee87d6295c244887b79e7b183751cb4d5
Andrey Zavodov <andrey0398@gmail.com>
2018-06-17 17:26:56 +0300

    Авторизация через ВК

--- cloud/views/authentication.py
+++ cloud/views/authentication.py
@@ -12,6 +12,15 @@ VK_API_VERSION = '5.74'
 VK_SECRET = 'loqcCdT0JDj5fTrQd0ku'
 
 
+def user_info_to_session(request, user):
+    request.session['user_avatar_url'] = UserInfo.objects.get(user=user).avatar.url
+    program = UserInfo.objects.get(user=user).program
+    if program is not None:
+        request.session['program_id'] = program.pk
+    else:
+        request.session['program_id'] = ""
+
+
 def sign_out(request):
     auth.logout(request)
     return redirect("index")
@@ -26,12 +35,7 @@ def sign_in(request):
         if user is not None and user.is_active:
             login(request, user)
 
-            request.session['user_avatar_url'] = UserInfo.objects.get(user=user).avatar.url
-            program = UserInfo.objects.get(user=user).program
-            if program is not None:
-                request.session['program_id'] = program.pk
-            else:
-                request.session['program_id'] = ""
+            user_info_to_session(request, user)
 
             return redirect('cloud')
         else:
@@ -63,16 +67,16 @@ def vk_auth(request):
     else:
         if code is not None:
             data = requests.get(URL).json()
-            token = data['access_token']
             vk_id = data['user_id']
-            user_info = UserInfo.objects.get(vk_id=vk_id)
-            if user_info is not None:
-                user = user_info.user
+            user_info = UserInfo.objects.filter(vk_id=vk_id)
+            if user_info.count():
+                user = user_info.first().user
                 user.backend = 'django.contrib.auth.backends.ModelBackend'
                 login(request, user)
+                user_info_to_session(request, user)
                 return redirect("cloud")
             else:
-                error = "К данной странице не привязан пользователь! Для начала зарегистрируйтесь."
+                error = "Ваша ВК старница не привязана ни к одному пользователю PiCloud! Для начала авторизуйтесь."
                 return render(request, 'auth/signin.html', {'error': error, 'host': request.get_host(), })
         else:
             error = request.GET('error_description')
