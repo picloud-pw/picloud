4a99e74a63cca88b98fe28b08e5488dad741ba2f
Egor Dubenetskiy <egor@banka.space>
2018-11-16 23:28:18 +0300

    Редирект на /feed с корня

--- cloud/views/index.py
+++ cloud/views/index.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect('cloud')
+        return redirect('feed')
     else:
         return redirect('about')
 
