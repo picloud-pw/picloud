1c5f985fec4ed4b032797ff383efc45b1b216a02
Andrey Zavodov <andrey.zavodov@jetbrains.com>
2019-09-21 17:49:32 +0300

    [memes] Implement model

--- memes/models.py
+++ memes/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
-from social.tests.models import User
+
+from hierarchy.models import Department
 
 
-class MemeSource(models.Model):
+class MemesSource(models.Model):
     source = models.URLField(null=False, blank=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    department = models.ForeignKey(Department, null=True, blank=True, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.source
---
+++ memes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from hierarchy import views
+
+
+urlpatterns = [
+
+]
