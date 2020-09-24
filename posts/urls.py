5c2cc0c009b03cefc0b8b577daae3fb2a0e0b2f6
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-09-24 16:46:22 +0300

    [posts] Implement endpoint for posts searching

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
+
+from posts import views
 
 app_name = 'posts'
 
 urlpatterns = [
+
+    path('search', views.search_posts,),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from posts.models import Post
+
+
+def search_posts(request):
+    is_approved = request.GET.get('is_approved') in ['True', None]
+
+    posts = Post.objects.all()
+
+    if is_approved or (not is_approved and request.user.is_superuser):
+        posts = posts.filter(is_approved=is_approved)
+
+    return JsonResponse([
+        post.as_dict() for post in posts
+    ], safe=False)
