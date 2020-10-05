a00c7f931ccf20e5868449c2f934404986994954
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-10-05 23:50:31 +0300

    [hierarchy] Refactor and add new endpoints

--- hierarchy/urls.py
+++ hierarchy/urls.py
@@ -5,7 +5,9 @@ app_name = 'hierarchy'
 
 urlpatterns = [
 
+    path('departments/types', views.departments.get_department_types,),
     path('departments/search', views.departments.search_departments,),
+    path('departments/get', views.departments.get_department,),
 
     path('subjects/search', views.subjects.search_subjects, ),
 
--- hierarchy/views/departments.py
+++ hierarchy/views/departments.py
@@ -10,27 +10,42 @@ def get_department_types(request):
     ], safe=False)
 
 
+def get_department(request):
+    department_id = request.GET.get('department_type_id')
+
+    department = Department.objects.get(id=department_id)
+
+    return JsonResponse({
+        'department': department.as_dict(),
+        'hierarchy': {},
+        'statistics': {},
+    })
+
+
 def search_departments(request):
     query = request.GET.get('q')
     department_type_id = request.GET.get('department_type_id')
-    parent_department_id = request.GET.get('department_id')
+    parent_department_id = request.GET.get('parent_department_id')
     is_approved = request.GET.get('is_approved') in ['True', None]
 
     departments = Department.objects.all()
 
     if query is not None:
         departments = departments.filter(name__icontains=query)
-    if parent_department_id is not None:
+    if department_type_id is not None:
         try:
-            department = Department.objects.get(id=parent_department_id)
-            departments = departments.filter(parent_department=department)
+            department_type = DepartmentType.objects.get(id=department_type_id)
+            departments = departments.filter(department_type=department_type)
         except ObjectDoesNotExist:
             return JsonResponse({
                 'error': f'DepartmentType with id={department_type_id} does not exist'
             })
     if parent_department_id is not None:
         try:
-            department = Department.objects.get(id=parent_department_id)
+            if parent_department_id != 'null':
+                department = Department.objects.get(id=parent_department_id)
+            else:
+                department = None
             departments = departments.filter(parent_department=department)
         except ObjectDoesNotExist:
             return JsonResponse({
@@ -39,4 +54,6 @@ def search_departments(request):
     if is_approved or (not is_approved and request.user.is_superuser):
         departments = departments.filter(is_approved=is_approved)
 
-    return JsonResponse([d.as_dict() for d in departments], safe=False)
+    return JsonResponse({
+        'departments': [d.as_dict() for d in departments]
+    })
