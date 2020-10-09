8ac72d5e3fd6c6a2c69765175a38799b4dfecef6
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-10-09 05:42:50 +0300

    [website] Add Date formatting methods

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,3 +1,31 @@
 function random_ID() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
+
+Date.prototype.toPrettyString = function () {
+    let dd = this.getDate();
+    let mm = this.getMonth() + 1;
+
+    let date = [
+        (dd > 9 ? '' : '0') + dd,
+        (mm > 9 ? '' : '0') + mm,
+        this.getFullYear(),
+    ].join('-');
+    let time = [
+        this.getHours(),
+        this.getMinutes(),
+    ].join(':');
+    return `${date}${time === '3:0' ? '' : ' ' + time}`;
+};
+
+Date.prototype.toYYYYMMDD = function () {
+
+    let dd = this.getDate();
+    let mm = this.getMonth() + 1;
+
+    return [
+        this.getFullYear(),
+        (mm > 9 ? '' : '0') + mm,
+        (dd > 9 ? '' : '0') + dd,
+    ].join('-');
+};
