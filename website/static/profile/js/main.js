c553e919276046c3c93ef406943a87616712d21f
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-10-15 06:46:16 +0300

    [website][profile] Add personal info edit

--- website/static/profile/js/main.js
+++ website/static/profile/js/main.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+let USER_INFO = {};
+
 function init_personal_info() {
     let container = document.getElementById('personal_info_container');
     container.classList.add('loading');
@@ -11,6 +13,7 @@ function init_personal_info() {
     axios.get('/students/me')
         .then((response) => {
             let me = response.data;
+            USER_INFO = me;
 
             init_user_department(me['department']);
             container.innerHTML = `
@@ -26,10 +29,13 @@ function init_personal_info() {
                     <img src="${me['avatar']}" alt="avatar" style="padding: 30px; background-color: #fff">
                 </div>
                 <div class="content">
+                  <a class="right floated"> 
+                     <i class="ui pencil icon" onclick="init_edit_form('username')"></i> 
+                  </a>
                   <div class="header">${me['user']['username']}</div>
                   <div class="meta">
                     <span class="date">Last login at ${new Date(me['user']['last_login']).toPrettyString()}</span>
-                  </div>
+                  </div>         
                 </div>
                 <div class="content">
                   <span title="Status">
@@ -42,14 +48,11 @@ function init_personal_info() {
                 <div class="content">
                   <i class="user icon"></i> ${me['user']['first_name']} ${me['user']['last_name']}
                   <a class="right floated"> 
-                     <i class="ui pencil icon"></i> 
+                     <i class="ui pencil icon" onclick="init_edit_form('name')"></i> 
                   </a>
                 </div>
                 <div class="content">
                   <i class="at icon"></i> ${me['user']['email']}
-                  <a class="right floated"> 
-                      <i class="ui pencil icon"></i>
-                  </a>
                 </div>
               </div>
             `;
@@ -76,7 +79,7 @@ function init_user_department(department) {
                 <i class="university icon"></i>
                 You haven't chosen a department yet.
               </div>
-              <div class="ui basic button">Choose department</div>
+              <div class="ui basic button" onclick="init_edit_form('department')">Choose department</div>
             </div>
         `;
         return;
@@ -104,4 +107,70 @@ function init_user_department(department) {
 
 }
 
+
+function init_edit_form(field) {
+    let fields = '';
+
+    if (field === 'name') {
+        fields = `
+            <div class="field">
+                <label>First name</label>
+                <input class="ui fluid input" name="first_name" value="${USER_INFO['user']['first_name']}"/>
+            </div>
+            <div class="field">
+                <label>Last name</label>
+                <input class="ui fluid input" name="last_name" value="${USER_INFO['user']['last_name']}"/>
+            </div>
+        `;
+    }
+
+    if (field === 'username') {
+        fields = `
+            <div class="field">
+                <label>Username</label>
+                <input class="ui fluid input" name="username" value="${USER_INFO['user']['username']}"/>
+            </div>
+        `;
+    }
+
+    let container = document.getElementById('edit_form_modal_container');
+    container.innerHTML = `
+        <div class="ui tiny modal" id="edit_form_modal">
+          <i class="close icon"></i>
+          <div class="header">Edit personal information</div>
+          <div class="content">
+            <form class="ui form" id="edit_form">
+                ${fields}
+            </form>
+          </div>
+          <div class="actions">
+            <div class="ui black deny button">Cancel</div>
+            <div class="ui positive right labeled icon button"> Save <i class="checkmark icon"></i> </div>
+          </div>
+        </div>
+    `;
+
+    $('#edit_form_modal')
+        .modal({
+            onApprove: (elem) => {
+                save_edit_form();
+            },
+        })
+        .modal('show');
+}
+
+function save_edit_form() {
+    let formData = new FormData(
+        document.getElementById('edit_form')
+    );
+
+    axios.post(`/students/me/edit`, formData, {headers: {'X-CSRFToken': Cookies.get('csrftoken')}})
+        .then(response => {
+            show_alert(response.data['status'], response.data['message']);
+            init_personal_info();
+        })
+        .catch(error => {
+            show_alert('error', error);
+        })
+}
 
