522e6bb5bf7213c8b61fc05517a76f193f461991
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-10-24 21:47:40 +0300

    [website][students] Refactor page

--- website/static/students/js/main.js
+++ website/static/students/js/main.js
@@ -11,25 +11,38 @@ document.addEventListener("DOMContentLoaded", function () {
 let PAGE = 1;
 
 function display_student_info(student_id) {
-    console.log(student_id);
+    let container = document.getElementById("students_container");
+    container.innerHTML = `
+        ${student_id}
+    `;
 }
 
 function init_students_list() {
     PAGE = 1;
 
     let rc = document.getElementById("students_container");
+    rc.innerHTML = `
+        <div class="ui basic segment divided items" id="students_list"></div>
+        <div class="ui centered button" id="load_button">
+            Load more
+        </div>
+    `;
+    let students_list_container = document.getElementById('students_list');
+
     rc.addEventListener('scroll', function () {
         if (rc.scrollHeight - rc.scrollTop <= rc.clientHeight) {
-            load_students();
+            load_students(students_list_container);
         }
     });
+    document.getElementById('load_button').onclick = () => {
+        load_students(students_list_container);
+    }
 
-    load_students();
+    load_students(students_list_container);
 
 }
 
-function load_students() {
-    let container = document.getElementById('students_container');
+function load_students(container) {
     let load_button = document.getElementById('load_button');
     load_button.classList.add('loading');
     axios.get(`/students/search?p=${PAGE}`)
