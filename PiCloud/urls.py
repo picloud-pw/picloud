55a9acf8159c163ae98617b3fa73066dd9f19c1e
Andrey Zavodov <a.p.zavodov@gmail.com>
2020-11-19 23:56:12 +0300

    [search] Implement base 'search' endpoint

--- PiCloud/urls.py
+++ PiCloud/urls.py
@@ -1,30 +1,19 @@
-"""PiCloud URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('', include('social_django.urls', namespace='social')),
     path('', include('cloud.urls')),
     path('', include('website.urls')),
+
     path('admin/', admin.site.urls, name='admin'),
     path('hierarchy/', include('hierarchy.urls', namespace='hierarchy')),
     path('memes/', include('memes.urls', namespace='memes')),
     path('posts/', include('posts.urls', namespace='posts')),
     path('students/', include('students.urls', namespace='students')),
+    path('search/', include('search.urls', namespace='search')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- search/views.py
+++ search/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import operator
+from functools import reduce
 
-# Create your views here.
+from django.db.models import Q
+from django.http import JsonResponse
+
+from posts.models import Post
+
+
+def search(request):
+    query = request.GET.get('q', None)
+
+    if query is None:
+        return JsonResponse([], safe=False)
+    query = " ".join(query.split())
+    query = query.strip()
+    words = query.split()
+
+    posts = Post.objects.filter(is_approved=True)
+    posts = posts.filter(reduce(operator.or_, (Q(title__icontains=x) for x in words)))
+    posts = posts.order_by('-created_date')[:10]
+
+    return JsonResponse({'results': {
+        'posts': [post.as_dict() for post in posts],
+    }})
