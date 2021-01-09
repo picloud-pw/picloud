8b133ded50f7353225a572622ecdeb5a492f2894
Andrey Zavodov <a.p.zavodov@gmail.com>
2021-01-09 23:31:18 +0300

    [website] 'new_post' page – add 'program' field

--- website/static/posts/js/new_post.js
+++ website/static/posts/js/new_post.js
@@ -6,18 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function init_page() {
+
     init_departments_search((result, response) => {
-            $("input[name='department_id']").val(result['department_id']);
-            display_department_hierarchy(
-                document.getElementById('search_department_hierarchy'),
-                result['department_id']
-            );
-        })
-    display_department_hierarchy(container, department_id)
+        $("input[name='department_id']").val(result['department_id']);
+        display_department_hierarchy(
+            document.getElementById('department_hierarchy'),
+            result['department_id']
+        );
+        init_subjects_list(result['department_id']);
+    })
 }
+
 
 function load_post_types() {
     let select = document.getElementById('post_type');
+    select.innerText = '';
     axios.get('/posts/types/get')
         .then(response => {
             let types = response.data['types'];
@@ -31,3 +34,25 @@ function load_post_types() {
 
 }
 
+
+function init_subjects_list(department_id) {
+    let select = document.getElementById('subject');
+    select.innerText = '';
+    axios.get(`/hierarchy/subjects/search?department_id=${department_id}`)
+        .then(response => {
+            let subjects = response.data['subjects'];
+            for (let s of subjects) {
+                select.innerHTML += `
+                    <option value="${s['id']}">
+                        ${s['name']} (sem. ${s['semester'] ? s['semester'] : '---'})
+                    </option>
+                `;
+            }
+            $('#subject').dropdown();
+        })
+}
+
+
+function submit_form() {
+
+}
