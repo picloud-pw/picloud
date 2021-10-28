dde4c4d7c15932687858b1d26326e49d2103ea6e
Andrey Zavodov <a.p.zavodov@gmail.com>
2021-10-28 04:14:37 +0300

    [website][chats] Add basic template

--- website/static/chats/js/main.js
+++ website/static/chats/js/main.js
@@ -5,5 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function restore_state() {
+    init_chat_lists_segment();
+    init_chat_content_segment();
+    init_chat_bar_segment();
+}
+
+function init_chat_lists_segment() {
+    load_chat_lists();
+    document.getElementById('chat_list').innerHTML = `
+        <div class="ui top segment"></div>
+    `;
+}
+
+function init_chat_content_segment() {
+    document.getElementById('chat_content').innerHTML = `
+        <div class="ui top segment"></div>
+        <div class="ui bottom segment"></div>
+    `;
+}
+
+function init_chat_bar_segment() {
+    document.getElementById('chat_bar').innerHTML = `
+        <div class="ui top segment"></div>
+    `;
+}
+
+function load_chat_lists() {
 
 }
