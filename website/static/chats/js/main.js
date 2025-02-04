0c45088881fb94f6a08231d0c0681a92d67f53c1
Andrey Zavodov <a.p.zavodov@gmail.com>
2025-02-05 00:50:16 +0200

    [deploy] Add portainer config

--- website/static/chats/js/main.js
+++ website/static/chats/js/main.js
@@ -419,6 +419,31 @@ export class Chats {
         `;
     }
 
+    display_message_context_menu() {
+        document.onclick = hideMenu;
+        document.oncontextmenu = rightClick;
+
+        function hideMenu() {
+            document.getElementById("contextMenu").style.display = "none"
+        }
+
+        function rightClick(e) {
+            e.preventDefault();
+
+            if (document.getElementById(
+                "contextMenu").style.display == "block")
+                hideMenu();
+            else {
+                let menu = document
+                    .getElementById("contextMenu")
+
+                menu.style.display = 'block';
+                menu.style.left = e.pageX + "px";
+                menu.style.top = e.pageY + "px";
+            }
+        }
+    }
+
     open_chat_lists_sidebar() {
         let el_id = random_ID();
         $.flyout({
